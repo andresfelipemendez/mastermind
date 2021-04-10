b7b69cfa179687fdfff0faf9b1f6d4dd9941f8e9
Andrés Felipe <fmendez1212@gmail.com>
2021-04-10 00:25:16 -0500

    score finished

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -11,8 +11,19 @@
   (count-true 
       (map #(contains? (set code) %1) guess)))
 
+(defn count-of [value values]
+  (count (filter #(= value %) values)))
+
+(defn over-count [code guess]
+  (let [code-values (set code)]
+  (->> code-values
+    (map #(- (count-of % guess) (count-of % code)))
+    (filter pos?)
+    (reduce +))))
+
 (defn score [code guess]
   (let [p (position-matches code guess)
-        v (value-matches code guess)]
-  [p (- v p)])
+        v (value-matches code guess)
+        o (over-count code guess)]
+  [p (- v p o)])
 )
--- test/mastermind/core_test.clj
+++ test/mastermind/core_test.clj
@@ -26,3 +26,10 @@
   (fact
     "one value match"
     (score [1 2 3 4] [2 0 0 0])=>[0 1]))
+
+
+  (fact
+    "confound 1, if there are duplicated colours in the guess, they cannot all be awarded a key peg unless they correspond to the same number of duplicate colours in the hidden code."
+    (score [1 2 3 4] [3 3 3 4])=>[2 0]
+    (score [1 1 2 3] [1 2 1 2])=>[1 2]
+    (score [3 3 3 3] [3 1 1 1])=>[1 0])
